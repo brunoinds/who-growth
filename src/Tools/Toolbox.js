b8415d19aefbf83600ff9337805ef474f561827a
Bruno Freire <brunoqm99@gmail.com>
2023-07-04 21:07:29 -0500

    Update

--- src/Tools/Toolbox.js
+++ src/Tools/Toolbox.js
@@ -1,4 +1,8 @@
-import esMain from 'es-main'
+import path from 'path';
+import process from 'process';
+import {createRequire} from 'module';
+import {fileURLToPath} from 'url';
+
 class Toolbox{
     static generateID(){
         let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
@@ -11,6 +15,28 @@ class Toolbox{
     static getExecutionType(){
         function isModule() {
             return typeof module !== 'undefined' && module.exports !== undefined;
+        }
+        function esMain(meta) {
+            function stripExt(name) {
+                const extension = path.extname(name);
+                if (!extension) {
+                    return name;
+                }
+                return name.slice(0, -extension.length);
+            }
+            if (!meta || !process.argv[1]) {
+                return false;
+            }
+
+            const require = createRequire(meta.url);
+            const scriptPath = require.resolve(process.argv[1]);
+            const modulePath = fileURLToPath(meta.url);
+
+            const extension = path.extname(scriptPath);
+            if (extension) {
+                return modulePath === scriptPath;
+            }
+            return stripExt(modulePath) === scriptPath;
         }
 
         if (isModule()){
